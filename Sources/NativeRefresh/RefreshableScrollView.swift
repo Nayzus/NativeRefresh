48e7e7208f996c4e8099268f2f12fb1d656dc3c2
Pavel Parshutkin <parshutkin.p@orionnet.ru>
2022-08-17 09:27:11 +0700

    ...

--- Sources/NativeRefresh/RefreshableScrollView.swift
+++ Sources/NativeRefresh/RefreshableScrollView.swift
@@ -3,7 +3,8 @@ import Combine
 
 
 public struct RefreshableScrollView<Content: View>: View {
-    @ObservedObject private var configuration: RefreshControlStyleConfiguration = .init()
+    @PersistentObject private var configuration: RefreshControlStyleConfiguration = .init()
+    
     @State private var currentOffset: CGFloat = 0.0 {
         willSet {
             print(currentOffset)
@@ -103,4 +104,94 @@ private extension RefreshableScrollView {
 private struct OffsetPreferenceKey: PreferenceKey {
     static var defaultValue: CGPoint = .zero
     static func reduce(value: inout CGPoint, nextValue: () -> CGPoint) { }
+}
+
+
+@propertyWrapper
+public struct PersistentObject<ObjectType: ObservableObject>: DynamicProperty {
+    private let thunk: () -> ObjectType
+    
+    @State private var objectContainer = _OptionalObservedObjectContainer<ObjectType>()
+    
+    @ObservedObject private var observedObjectContainer = _OptionalObservedObjectContainer<ObjectType>()
+    
+    public var wrappedValue: ObjectType {
+        get {
+            if let object = objectContainer.base {
+                if observedObjectContainer.base !== object {
+                    observedObjectContainer.base = object
+                }
+                
+                return object
+            } else {
+                let object = thunk()
+                
+                objectContainer.base = object
+                observedObjectContainer.base = object
+                
+                return object
+            }
+        } nonmutating set {
+            objectContainer.base = newValue
+            observedObjectContainer.base = newValue
+        }
+    }
+    
+    public var projectedValue: ObservedObject<ObjectType>.Wrapper {
+        ObservedObject(wrappedValue: wrappedValue).projectedValue
+    }
+    
+    public init(wrappedValue thunk: @autoclosure @escaping () -> ObjectType) {
+        self.thunk = thunk
+    }
+    
+    public mutating func update() {
+        _objectContainer.update()
+        _observedObjectContainer.update()
+    }
+}
+
+
+final class _OptionalObservedObjectContainer<ObjectType: ObservableObject>: ObservableObject {
+    private var baseSubscription: AnyCancellable?
+    
+    var onObjectWillChange: () -> Void = { }
+    
+    var base: ObjectType? {
+        didSet {
+            if let oldValue = oldValue, let base = base {
+                if oldValue === base, baseSubscription != nil {
+                    return
+                }
+            }
+            
+            subscribe()
+        }
+    }
+    
+    init(base: ObjectType? = nil) {
+        self.base = base
+        
+        subscribe()
+    }
+    
+    private func subscribe() {
+        guard let base = base else {
+            return
+        }
+        
+        baseSubscription = base
+            .objectWillChange
+            .receive(on: DispatchQueue.main)
+            .sink(receiveValue: { [weak self] _ in
+                guard let `self` = self else {
+                    return
+                }
+                
+                DispatchQueue.main.async {
+                    `self`.objectWillChange.send()
+                    `self`.onObjectWillChange()
+                }
+            })
+    }
 }
